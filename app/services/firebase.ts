eb25c79abe4d6d1fad0747a4ba80c26783d978d2
Giovanni Chagas <giovanni0012@hotmail.com>
2025-12-25 19:22:32 +0100

    Add README

--- app/services/firebase.ts
+++ app/services/firebase.ts
@@ -1,11 +1,12 @@
-// Import the functions you need from the SDKs you need
-import { getAnalytics } from "firebase/analytics";
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Platform } from "react-native";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+import { getAuth, initializeAuth } from "firebase/auth";
+//import { getReactNativePersistence } from "firebase/auth/react-native";
+// criando o commit correto
 const firebaseConfig = {
   apiKey: "AIzaSyBNXQdnaaATvFgJ0mmDpeCJ1UOi-d-EH0w",
   authDomain: "alercheck.firebaseapp.com",
@@ -13,9 +14,15 @@ const firebaseConfig = {
   storageBucket: "alercheck.firebasestorage.app",
   messagingSenderId: "389174712778",
   appId: "1:389174712778:web:b81ba1927002af2f8f1733",
-  measurementId: "G-PQ8ECZ6CHN"
+  measurementId: "G-PQ8ECZ6CHN",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+export const auth =
+  Platform.OS === "web"
+    ? getAuth(app)
+    : initializeAuth(app, {
+        persistence: getReactNativePersistence(AsyncStorage),
+      });
